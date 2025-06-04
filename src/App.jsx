70e293ddcd93cf3753d21eb4ad22b9431e13627d
StacyNComer <comerstacy@gmail.com>
2025-06-04 13:26:47 -0500

    Added Personalizations

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   return (
     <>
@@ -17,9 +17,10 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>Also, hi Raj!</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setCount((count) => count * 2)}>
+          Multiply: {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
